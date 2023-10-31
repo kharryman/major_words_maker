3087e9d4d12f0cad8a9b86fde7640ceab07729c1
kharryman <keithharryman75@gmail.com>
2023-10-30 21:26:54 -0700

    0.0.4 android

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 CALL flutter clean
 CALL flutter pub get
-CALL copy /y ..\major_maker_key.properties .\android
+CALL copy /y ..\flutter_key.properties .\android
 CALL copy /y ..\lfq.keystore .\android\app
 CALL del /q /S ".\build\app\outputs\bundle\release\app-release.aab"
 CALL flutter build appbundle --release
@@ -8,5 +8,5 @@ CALL cd .\build\app\outputs\bundle\release
 CALL del /q /S "major-maker-release.aab"
 CALL zipalign -v 4 app-release.aab major-maker-release.aab
 CALL cd ..\..\..\..\.. 
-CALL del /q /S "android\major_maker_key.properties"
+CALL del /q /S "android\flutter_key.properties"
 CALL del /q /S "android\app\lfq.keystore"
--- run_release.bat
+++ run_release.bat
@@ -1,11 +1,11 @@
 CALL flutter clean
 CALL flutter pub get
-CALL copy /y ..\major_maker_key.properties .\android
+CALL copy /y ..\flutter_key.properties .\android
 CALL copy /y ..\lfq.keystore .\android\app
 CALL del /q /S ".\build\app\outputs\flutter-apk\app-release.apk"
 CALL flutter build apk --release
 CALL cd .\build\app\outputs\flutter-apk
 CALL adb install app-release.apk
 CALL cd ..\..\..\..
-CALL del /q /S "android\major_maker_key.properties"
+CALL del /q /S "android\flutter_key.properties"
 CALL del /q /S "android\app\lfq.keystore"
